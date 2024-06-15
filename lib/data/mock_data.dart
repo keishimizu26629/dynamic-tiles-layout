772559dcb9c061cd39461a179d5178668152ba61
keishimizu26629 <keisukeshimizu.inoworl@gmail.com>
2024-06-16 05:56:15 +0900

    fix: change mock data

--- lib/data/mock_data.dart
+++ lib/data/mock_data.dart
@@ -3,21 +3,52 @@ import '../domain/post.dart';
 Future<List<Post>> fetchMockPosts() async {
   return [
     Post(
-      userName: 'John Doe',
-      userImageUrl: 'https://via.placeholder.com/150',
-      content: 'This is a mock post content.',
+      userName: 'Jane Smith',
+      userImageUrl: 'https://via.placeholder.com/150/00FFFF',
+      content: 'This is one image post.',
       postImageUrls: [
         'https://via.placeholder.com/600',
+      ],
+    ),
+    Post(
+      userName: 'Jane Smith',
+      userImageUrl: 'https://via.placeholder.com/150/00FFFF',
+      content: 'This is two images post.',
+      postImageUrls: [
+        'https://via.placeholder.com/600',
+        'https://via.placeholder.com/600/FFF000',
+      ],
+    ),
+    Post(
+      userName: 'Jane Smith',
+      userImageUrl: 'https://via.placeholder.com/150/00FFFF',
+      content: 'This is a 3 images post.',
+      postImageUrls: [
+        'https://via.placeholder.com/600',
+        'https://via.placeholder.com/600/FFF000',
+        'https://via.placeholder.com/600/FF0000',
+      ],
+    ),
+    Post(
+      userName: 'Jane Smith',
+      userImageUrl: 'https://via.placeholder.com/150/00FFFF',
+      content: 'Another mock post content.',
+      postImageUrls: [
+        'https://via.placeholder.com/600',
+        'https://via.placeholder.com/600/FFF000',
+        'https://via.placeholder.com/600/FF0000',
         'https://via.placeholder.com/600/0000FF',
       ],
     ),
     Post(
       userName: 'Jane Smith',
-      userImageUrl: 'https://via.placeholder.com/150',
+      userImageUrl: 'https://via.placeholder.com/150/00FFFF',
       content: 'Another mock post content.',
       postImageUrls: [
         'https://via.placeholder.com/600',
+        'https://via.placeholder.com/600/FFF000',
         'https://via.placeholder.com/600/FF0000',
+        'https://via.placeholder.com/600/005511',
       ],
     ),
   ];
